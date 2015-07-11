0d27174797f3d8bfe35e537ed4f0b59db0f3db9d
alexax66 <alexax66@gmail.com>
2015-07-11 20:36:43 +0000

    Add build_kernel.sh script

---
+++ build_kernel.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ ! -d $(pwd)/output ]; 
+    then
+	mkdir $(pwd)/output; 
+    fi
+
+make -C $(pwd) O=output alexax_defconfig  VARIANT_DEFCONFIG=msm8930_serrano_eur_lte_defconfig SELINUX_DEFCONFIG=selinux_defconfig
+
+make -j5 -C $(pwd) O=output
+
+cp $(pwd)/output/arch/arm/boot/zImage $(pwd)/arch/arm/boot/zImage
+
+if [ ! -d $(pwd)/output/modules ]; 
+    then
+	mkdir $(pwd)/output/modules; 
+    fi
+find $(pwd)/output -name '*.ko' -exec cp -v {} $(pwd)/output/modules \;
+
+exit
