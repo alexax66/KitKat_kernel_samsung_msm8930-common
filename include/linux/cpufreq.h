091dcaba860666e83b818c188cb0f09932fffe80
alexax66 <alexax66@gmail.com>
2015-07-24 16:13:08 +0000

    cpufreq: added Preservative CPU governor
    Preservative is based on the idea that the CPU will consume a lot of power
    when it changes frequency. It is based on the conservative governor.
    The idea is that it will stay at the step specified (702MHz selected
    by the creator Bedalus) unless needed. You will notice it will hover
    around 702 a lot, and not go above too much, and only to min freq
    when NOTHING is happening at all. This is most beneficial when you
    are doing something like reading; the screen is static or playing
    light games that won't need boosting any more
    The governor comes from Moob kernel for nexus 4

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -449,6 +449,9 @@ extern struct cpufreq_governor cpufreq_gov_impulse;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LIONHEART)
 extern struct cpufreq_governor cpufreq_gov_lionheart;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_lionheart)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_PRESERVATIVE)
+extern struct cpufreq_governor cpufreq_gov_preservative;
+#define CPUFREQ_DEFAULT_GOVERNOR<------>(&cpufreq_gov_preservative)
 #endif
 
 /*********************************************************************
