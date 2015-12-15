4e9a12af8cf92fd3e7d24dd703b4b3b078b7201f
alexax66 <alexax66@gmail.com>
2015-12-15 15:11:56 +0000

    sleepy_plug: upgrade to 3.0 version
    Based on commits:
     https://github.com/rmbq/android_kernel_htc_msm8960/commit/feaee8bc603be216aeaaeba84b758aae9b5762a9
     https://github.com/rmbq/android_kernel_htc_msm8960/commit/57a3f29e48d8980d6dbb4b75b81cae7d448471ef
     https://github.com/rmbq/android_kernel_htc_msm8960/commit/283fed27f9770affe285062fd26c87791e664756
     https://github.com/rmbq/android_kernel_htc_msm8960/commit/64c6ee06c420e48a66b4b9e36064f30f7587a368
     https://github.com/rmbq/android_kernel_htc_msm8960/commit/07712b5b7b333faa0429232c16f7cf6fea0d62a3
     https://github.com/rmbq/android_kernel_htc_msm8960/commit/476ccc32da761177c34024fc737236bb2f3bd86d
     https://github.com/rmbq/android_kernel_htc_msm8960/commit/9297f716fd82ca77bc8e1ddfe2dee8cdc82f6d47
     https://github.com/rmbq/android_kernel_htc_msm8960/commit/32d3422655851fa209252dad353856eae49f5256
     https://github.com/rmbq/android_kernel_htc_msm8960/commit/e4b7253dfacb533e6272bfa924deb287e7a730e1
     https://github.com/rmbq/android_kernel_htc_msm8960/commit/428a5d17d1d6e4f098462a7bdebc47bb9f3ab047

--- arch/arm/mach-msm/sleepy_plug.c
+++ arch/arm/mach-msm/sleepy_plug.c
@@ -1,7 +1,7 @@
 /*
  * Author: Davide Rombolà aka rmbq <davide.rombola@gmail.com>
  *
- * sleepy_plug: cpu_hotplug driver based on sleepy_plug by faux123
+ * sleepy_plug: cpu_hotplug driver based on intelli_plug by faux123
  *
  * Copyright 2012 Paul Reioux
  * Copyright 2014 Davide Rombolà
@@ -29,53 +29,70 @@
 #include <linux/earlysuspend.h>
 #endif
 
+extern unsigned int report_load_at_max_freq(void);
+
 //#define DEBUG_SLEEPY_PLUG
 #undef DEBUG_SLEEPY_PLUG
 
-#define SLEEPY_PLUG_MAJOR_VERSION	1
-#define SLEEPY_PLUG_MINOR_VERSION	5
+#define SLEEPY_PLUG_MAJOR_VERSION	3
+#define SLEEPY_PLUG_MINOR_VERSION	0
 
 #define DEF_SAMPLING_MS			(1000)
 #define BUSY_SAMPLING_MS		(500)
+#define MIN_SAMPLING_MS			(250)
 
-#define DOWN_THRESHOLD			8
-#define UP_THRESHOLD			11
+#define CPU_LOAD_THRESHOLD		90
+#define CPU_OVER_LOAD_THRESHOLD	2500
+
 #define PEAK_THRESHOLD			30
 
-#define RQ_VALUE_ARRAY_DIM		5
+#define RQ_VALUES_ARRAY_DIM		5
 
 static DEFINE_MUTEX(sleepy_plug_mutex);
 
 struct delayed_work sleepy_plug_work;
 
 static struct workqueue_struct *sleepy_plug_wq;
+static cputime64_t last_time, last_cpu_time_over_th;
+static long total_cpu_time_over_th;
 
 enum mp_decisions {
-	DO_NOTHING,
-	CPU_UP,
-	CPU_DOWN
+	DO_NOTHING = 0,
+	ONE_CPU_UP = 1,
+	TWO_CPU_UP = 2,
+#if CONFIG_NR_CPUS == 4
+	THREE_CPU_UP = 3,
+	FOUR_CPU_UP = 4,
+#endif
 };
 
-static unsigned int sleepy_plug_active = 0;
-module_param(sleepy_plug_active, uint, 0644);
+#if CONFIG_NR_CPUS == 2
+static unsigned int up_thresholds[3] = {0, 11, UINT_MAX};
+static unsigned int down_thresholds[3] = {0, 8, PEAK_THRESHOLD};
+#else
+static unsigned int up_thresholds[5] = {0, 11, 18, 27, UINT_MAX};
+static unsigned int down_thresholds[5] = {0, 8, 15, 24, PEAK_THRESHOLD};
+#endif
 
-static unsigned int sampling_time = 0;
+static unsigned int sleepy_plug_active = 0;
+
+static unsigned int sampling_time = DEF_SAMPLING_MS;
 static bool suspended = false;
-static unsigned int rq_values[RQ_VALUE_ARRAY_DIM] = {6};
+static unsigned int rq_values[RQ_VALUES_ARRAY_DIM] = {PEAK_THRESHOLD};
 
 static int calc_rq_avg(int last_rq_depth) {
 	int i;
 	int avg = 0;
 
 	//shift all values by 1
-	for(i = 0;i < RQ_VALUE_ARRAY_DIM-1;i++) {
+	for(i = 0;i < RQ_VALUES_ARRAY_DIM-1;i++) {
 		rq_values[i] = rq_values[i+1];
 		avg += rq_values[i+1];
 	}
 	avg += last_rq_depth;
-	rq_values[RQ_VALUE_ARRAY_DIM-1] = last_rq_depth;
+	rq_values[RQ_VALUES_ARRAY_DIM-1] = last_rq_depth;
 
-	return avg/RQ_VALUE_ARRAY_DIM;
+	return avg/RQ_VALUES_ARRAY_DIM;
 }
 
 static enum mp_decisions mp_decision(void)
@@ -84,55 +101,82 @@ static enum mp_decisions mp_decision(void)
 	int avg,i;
 	enum mp_decisions decision = DO_NOTHING;
 
+	avg = calc_rq_avg(rq_info.rq_avg);
+	
 	if(rq_info.rq_avg > PEAK_THRESHOLD) {
-		for(i = 0;i < RQ_VALUE_ARRAY_DIM-1;i++) 
-			rq_values[i] = rq_values[i+1];
-		rq_values[RQ_VALUE_ARRAY_DIM-1] = rq_info.rq_avg;
-
-		avg = rq_info.rq_avg;
+#if CONFIG_NR_CPUS == 2
+		decision = TWO_CPU_UP;
+#else
+		decision = FOUR_CPU_UP;
+#endif
 	}
-	else
-		avg = calc_rq_avg(rq_info.rq_avg);
-	nr_cpu_online = num_online_cpus();
+	else {
+		nr_cpu_online = num_online_cpus();
 
-	if(nr_cpu_online == 1 && avg >= UP_THRESHOLD)
-		decision = CPU_UP;
-	else if(nr_cpu_online == 2 && avg < DOWN_THRESHOLD)
-		decision = CPU_DOWN;
+		for(i = 1; i <= CONFIG_NR_CPUS; i++) {
+			if(avg >= down_thresholds[i - 1] && avg <= up_thresholds[i] && avg <= down_thresholds[i]) {
+				if(nr_cpu_online != i)
+					decision = i;
+				break;
+			}
+		}
 
+		if(decision == DO_NOTHING && nr_cpu_online != CONFIG_NR_CPUS && report_load_at_max_freq() > CPU_LOAD_THRESHOLD) {
+			cputime64_t this_time = ktime_to_ms(ktime_get());
+			if(last_cpu_time_over_th > 0) {
+				total_cpu_time_over_th += this_time - last_cpu_time_over_th;
+			}
+			if(total_cpu_time_over_th > CPU_OVER_LOAD_THRESHOLD) {
+				decision = nr_cpu_online + 1;
+			}
+
+			last_cpu_time_over_th = this_time;
+		}
+		else {
+			last_cpu_time_over_th = total_cpu_time_over_th = 0;
+		}
+	}
 #ifdef DEBUG_SLEEPY_PLUG
-	pr_info("[SLEEPY] nr_cpu_online: %d|avg: %d|max: %d|new? %d\n",nr_cpu_online,crav.avg,crav.max,crav.max_is_new == true?1:0);
+	pr_info("[SLEEPY] nr_cpu_online: %d|avg: %d\n",nr_cpu_online,avg);
 #endif
 	return decision;
 }
 
-static void __cpuinit sleepy_plug_work_fn(struct work_struct *work)
-{
-	enum mp_decisions decision = DO_NOTHING;
+static void __ref set_cpus(int n_cpus_on_requested) {
+	int nr_cpus_online = num_online_cpus();
 
-	if (sleepy_plug_active == 1) {
-		// detect artificial loads or constant loads
-		// using msm rqstats
-
-		decision = mp_decision();
-#ifdef DEBUG_SLEEPY_PLUG
-		pr_info("decision: %d\n",decision);
-#endif
-		if (!suspended) {
-			if (decision == CPU_UP) {
-				cpu_up(1);
-				sampling_time = BUSY_SAMPLING_MS;
-			} else if(decision == CPU_DOWN){
-				cpu_down(1);
-				sampling_time = DEF_SAMPLING_MS;
-			} else if(decision == DO_NOTHING)
-				sampling_time = DEF_SAMPLING_MS;
-		}
-#ifdef DEBUG_SLEEPY_PLUG
-		else
-			pr_info("sleepy_plug is suspened!\n");
-#endif
+	if(n_cpus_on_requested == DO_NOTHING) return;
+	
+	if(nr_cpus_online < n_cpus_on_requested) {
+		sampling_time = BUSY_SAMPLING_MS;
+		for(;nr_cpus_online < n_cpus_on_requested;nr_cpus_online++)
+			cpu_up(nr_cpus_online);
 	}
+	else{
+		for(nr_cpus_online--;nr_cpus_online >= n_cpus_on_requested - 1;nr_cpus_online--)
+			cpu_down(nr_cpus_online);
+	}
+}
+
+static void sleepy_plug_work_fn(struct work_struct *work)
+{
+#ifdef DEBUG_SLEEPY_PLUG
+	pr_info("decision: %d\n",decision);
+#endif
+	if (!suspended) {
+		cputime64_t this_time = ktime_to_ms(ktime_get());
+		if(this_time - last_time >= MIN_SAMPLING_MS) {
+			sampling_time = DEF_SAMPLING_MS;
+			set_cpus(mp_decision());
+			last_time = this_time;
+		}
+		else
+			return;
+	}
+#ifdef DEBUG_SLEEPY_PLUG
+	else
+		pr_info("sleepy_plug is suspened!\n");
+#endif
 	queue_delayed_work_on(0, sleepy_plug_wq, &sleepy_plug_work,
 		msecs_to_jiffies(sampling_time));
 }
@@ -140,16 +184,21 @@ static void __cpuinit sleepy_plug_work_fn(struct work_struct *work)
 #ifdef CONFIG_HAS_EARLYSUSPEND
 static void sleepy_plug_suspend(struct early_suspend *h)
 {
+	int nr_cpu_online;
+
 	flush_workqueue(sleepy_plug_wq);
 
 	mutex_lock(&sleepy_plug_mutex);
 	suspended = true;
 	mutex_unlock(&sleepy_plug_mutex);
 
-	cpu_down(1);
+	nr_cpu_online = num_online_cpus() - 1;
+	for(;nr_cpu_online > 0;nr_cpu_online--) {
+		cpu_down(nr_cpu_online);
+	}
 }
 
-static void __cpuinit sleepy_plug_resume(struct early_suspend *h)
+static void __ref sleepy_plug_resume(struct early_suspend *h)
 {
 	mutex_lock(&sleepy_plug_mutex);
 	/* keep cores awake long enough for faster wake up */
@@ -171,25 +220,9 @@ static struct early_suspend sleepy_plug_early_suspend_driver = {
 static void sleepy_plug_input_event(struct input_handle *handle,
 		unsigned int type, unsigned int code, int value)
 {
+	if(ktime_to_ms(ktime_get()) - last_time >= MIN_SAMPLING_MS)
         queue_delayed_work_on(0, sleepy_plug_wq, &sleepy_plug_work,
                 msecs_to_jiffies(10));
-}
-
-static int input_dev_filter(const char *input_dev_name)
-{
-	if (strstr(input_dev_name, "touchscreen") ||
-		strstr(input_dev_name, "sec_touchscreen") ||
-		strstr(input_dev_name, "touch_dev") ||
-		strstr(input_dev_name, "atmel_mxt224e") ||
-		strstr(input_dev_name, "-keypad") ||
-		strstr(input_dev_name, "-nav") ||
-		strstr(input_dev_name, "-oj")) {
-		pr_info("touch dev: %s\n", input_dev_name);
-		return 0;
-	} else {
-		pr_info("touch dev: %s\n", input_dev_name);
-		return 1;
-	}
 }
 
 static int sleepy_plug_input_connect(struct input_handler *handler,
@@ -197,9 +230,6 @@ static int sleepy_plug_input_connect(struct input_handler *handler,
 {
 	struct input_handle *handle;
 	int error;
-
-	if (input_dev_filter(dev->name))
-		return -ENODEV;
 
 	handle = kzalloc(sizeof(struct input_handle), GFP_KERNEL);
 	if (!handle)
@@ -233,7 +263,21 @@ static void sleepy_plug_input_disconnect(struct input_handle *handle)
 }
 
 static const struct input_device_id sleepy_plug_ids[] = {
-	{ .driver_info = 1 },
+	{
+		.flags = INPUT_DEVICE_ID_MATCH_EVBIT |
+			 INPUT_DEVICE_ID_MATCH_ABSBIT,
+		.evbit = { BIT_MASK(EV_ABS) },
+		.absbit = { [BIT_WORD(ABS_MT_POSITION_X)] =
+			    BIT_MASK(ABS_MT_POSITION_X) |
+			    BIT_MASK(ABS_MT_POSITION_Y) },
+	}, /* multi-touch touchscreen */
+	{
+		.flags = INPUT_DEVICE_ID_MATCH_KEYBIT |
+			 INPUT_DEVICE_ID_MATCH_ABSBIT,
+		.keybit = { [BIT_WORD(BTN_TOUCH)] = BIT_MASK(BTN_TOUCH) },
+		.absbit = { [BIT_WORD(ABS_X)] =
+			    BIT_MASK(ABS_X) | BIT_MASK(ABS_Y) },
+	}, /* touchpad */
 	{ },
 };
 
@@ -245,6 +289,56 @@ static struct input_handler sleepy_plug_input_handler = {
 	.id_table       = sleepy_plug_ids,
 };
 
+/********************* SYSF START *********************/
+static ssize_t sleepy_plug_active_show(struct device *dev, struct device_attribute *attr, char *buf) {
+	size_t count = 0;
+
+	count += sprintf(buf, "%d\n", sleepy_plug_active);
+
+	return count;
+}
+
+static ssize_t __ref sleepy_plug_active_store(struct device *dev, struct device_attribute *attr, const char *buf, size_t count) {
+	if (buf[0] >= '0' && buf[0] <= '1' && buf[1] == '\n')
+		if (sleepy_plug_active != buf[0] - '0') {
+			sleepy_plug_active = buf[0] - '0';
+
+			if(sleepy_plug_active == 0) {
+				int nr_cpu_online = CONFIG_NR_CPUS - 1;
+				flush_workqueue(sleepy_plug_wq);
+				for(;nr_cpu_online > 0;nr_cpu_online--)
+					cpu_up(nr_cpu_online);
+			} else {
+				queue_delayed_work_on(0, sleepy_plug_wq, &sleepy_plug_work,
+					msecs_to_jiffies(10));
+			}
+		}
+
+	return count;
+}
+static DEVICE_ATTR(sleepyplug, (S_IWUSR|S_IRUGO),
+	sleepy_plug_active_show, sleepy_plug_active_store);
+
+static struct kobject *android_touchkey_kobj;
+
+static int sleepyplug_sysfs_init(void) {
+	int ret;
+
+	android_touchkey_kobj = kobject_create_and_add("sleepy_plug", NULL);
+	if (android_touchkey_kobj == NULL) {
+		printk(KERN_ERR "%s: subsystem_register failed\n", __func__);
+		ret = -ENOMEM;
+		return ret;
+	}
+	ret = sysfs_create_file(android_touchkey_kobj, &dev_attr_sleepyplug.attr);
+	if (ret) {
+		printk(KERN_ERR "%s: sysfs_create_file failed\n", __func__);
+		return ret;
+	}
+
+	return 0;
+}
+/********************* SYSF END ***********************/
 int __init sleepy_plug_init(void)
 {
 	int rc;
@@ -262,6 +356,11 @@ int __init sleepy_plug_init(void)
 	register_early_suspend(&sleepy_plug_early_suspend_driver);
 #endif
 
+	sleepyplug_sysfs_init();
+
+	last_time = last_cpu_time_over_th = ktime_to_ms(ktime_get());
+	total_cpu_time_over_th = 0;
+
 	INIT_DELAYED_WORK(&sleepy_plug_work, sleepy_plug_work_fn);
 	queue_delayed_work_on(0, sleepy_plug_wq, &sleepy_plug_work,
 		msecs_to_jiffies(10));
@@ -275,4 +374,3 @@ MODULE_DESCRIPTION("'sleepy_plug' - An intelligent cpu hotplug driver for "
 MODULE_LICENSE("GPL");
 
 late_initcall(sleepy_plug_init);
-
